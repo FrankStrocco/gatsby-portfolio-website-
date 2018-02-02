2fea8ad5e1a2851be85b429136b942a4e0120b7a
younghee kwon <youngheekwon@younghees-MacBook-Pro.local>
2018-02-02 13:29:56 -0800

    pre deploy

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ module.exports = {
     author: "Aman Mittal",
     description: "A Gatsby.js Starter based on Ceevee by Styleshout"
   },
-  pathPrefix: `/project-name`,
+  pathPrefix: `/gatsby-portfolio-website-`,
   plugins: [
     {
       resolve: `gatsby-plugin-typography`,
